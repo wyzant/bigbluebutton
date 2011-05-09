07daaa7fc3319611af1df09cb7da740d510342af
deniszgonjanin <deniszgonjanin@gmail.com>
2011-05-09 19:34:21 +0000

    Continuing refactoring of bbb-api to java

--- bbb-api/src/main/java/org/bigbluebutton/api/Create.java
+++ bbb-api/src/main/java/org/bigbluebutton/api/Create.java
@@ -7,12 +7,22 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.bigbluebutton.api.util.Security;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Servlet Filter implementation class Create
  */
 public class Create implements Filter {
 
+	private static final String CALL_NAME = "create";
+	
+	Logger log = LoggerFactory.getLogger(Create.class);
+	
     /**
      * Default constructor. 
      */
@@ -31,9 +41,14 @@ public class Create implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-
+		if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)){
+			throw new RuntimeException("Only HTTP is supported by the BigBlueButton API");
+		}
+		
+		HttpServletRequest httpReq = (HttpServletRequest)request;
+		HttpServletResponse httpRes = (HttpServletResponse)response;
+		
+		Security.doChecksumSecurity(CALL_NAME, httpReq, httpRes);
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
@@ -42,7 +57,7 @@ public class Create implements Filter {
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		log.debug("/create filter started : " + fConfig.toString());
 	}
 
 }
---
+++ bbb-api/src/main/java/org/bigbluebutton/api/util/Security.java
@@ -0,0 +1,38 @@
+package org.bigbluebutton.api.util;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
+import org.bigbluebutton.api.Create;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Security {
+	
+	static final Logger log = LoggerFactory.getLogger(Security.class);
+	
+	public static Boolean doChecksumSecurity(String callName, HttpServletRequest request, HttpServletResponse response){
+		log.debug("Security: #doChecksumSecurity");
+		log.debug("checksum: " + request.getParameter("checksum") + "; query string: " + request.getQueryString());
+		
+		if (StringUtils.isEmpty(callName)){
+			throw new RuntimeException("Programming error - you must pass the call name to doChecksumSecurity so it can be used in the checksum");
+		}
+		if (StringUtils.isEmpty(request.getQueryString())){
+			invalid("noQueryString", "No query string was found in your request.", response);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static void invalid(String key, String message, HttpServletResponse response){
+		log.debug("Security : #invalid");
+		response.addHeader("Cache-Control", "no-cache");
+	}
+}
