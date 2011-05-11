3b963a1ef6eba87f2f112e1979efbce0615f5dc2
deniszgonjanin <deniszgonjanin@gmail.com>
2011-05-11 17:56:23 +0000

    Found an easy way to write Http request-response from Java, for testing.

--- bbb-api/src/main/java/org/bigbluebutton/api/util/Security.java
+++ bbb-api/src/main/java/org/bigbluebutton/api/util/Security.java
@@ -1,5 +1,7 @@
 package org.bigbluebutton.api.util;
 
+import java.io.IOException;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,12 +11,15 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.StringUtils;
 import org.bigbluebutton.api.Create;
+import org.bigbluebutton.api.xml.StandardResponse;
+import org.bigbluebutton.api.xml.XMLConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Security {
 	
 	static final Logger log = LoggerFactory.getLogger(Security.class);
+	static final XMLConverter xmlConverter = XMLConverter.getInstance();
 	
 	public static Boolean doChecksumSecurity(String callName, HttpServletRequest request, HttpServletResponse response){
 		log.debug("Security: #doChecksumSecurity");
@@ -33,6 +38,14 @@ public class Security {
 	
 	private static void invalid(String key, String message, HttpServletResponse response){
 		log.debug("Security : #invalid");
-		response.addHeader("Cache-Control", "no-cache");
+		try{
+			response.addHeader("Cache-Control", "no-cache");
+			response.setContentType("text/xml");
+			StandardResponse responseBody = new StandardResponse(StandardResponse.RESP_CODE_FAILED, key, message);
+			response.getWriter().println(xmlConverter.xml().toXML(responseBody));
+		} catch (IOException e){
+			
+		}
+		
 	}
 }
