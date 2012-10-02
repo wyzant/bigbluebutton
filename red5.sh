9ecf362cb7699772812dbed804efc15bfb56e7e9
wyzant <kyrian.willis@wyzant.com>
2012-10-02 09:10:00 -0500

    commit

---
+++ red5.sh
@@ -0,0 +1,6 @@
+# Add the BigBlueButton key
+wget http://ubuntu.bigbluebutton.org/bigbluebutton.asc -O- | sudo apt-key add -
+
+# Add the BigBlueButton repository URL and ensure the multiverse is enabled
+echo "deb http://ubuntu.bigbluebutton.org/lucid_dev_081/ bigbluebutton-lucid main" | sudo tee /etc/apt/sources.list.d/bigbluebutton.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ lucid multiverse" | sudo tee -a /etc/apt/sources.list
